a09a3afd36446d7edb147fa94c2cb05a87c53814
Kwon Sang <kwonsg514@gmail.com>
2023-09-05 17:26:42 +0900

    Refactor: 전면 개선, 최종 리팩토링

--- main.py
+++ main.py
@@ -1,45 +1,18 @@
-import os
-import itertools
-
+from modules import views
 from modules.board import Board
-from modules import commands, settings
 
 
 def run():
-    os.system(command='cls')
-    print(settings.MSG_GAME_START)
-    board = Board()
-    players = commands.player_name()
-    player_sequence = itertools.cycle(players.keys())
-
-    # Loop for turn in game
-    while not board.is_omok():
-        os.system(command='cls')
-        player_id = next(player_sequence)
-        board.show()
-        # Player put the stone. If that place is invalid, repeat util valid
-        print(f'Player : {players.get(player_id)}')
-        place = ''
-        while True:
-            try:
-                place = commands.place()
-            except ValueError:
-                print(settings.ERROR_PLACE)
-                continue
-            if place == settings.QUIT_KEY:
-                print(settings.MSG_QUIT)
-                return
-            if board.is_valid_place(place):
-                break
-            else:
-                print(settings.ERROR_PLACE)
-                continue
-        board.set_place(place)
-        board.put(player_id)
-        board.show()
-    os.system(command='cls')
-    board.show()
-    return print(f"\nPlayer '{players[player_id]}' is Win!!")
+    views.start_view()
+    while True:
+        views.stone_input_view(player_id=Board.current_player)
+        if Board.is_omok():
+            Board.show_board()
+            player_name = Board.players.get(Board.current_player)[0]
+            player_stone = Board.players.get(Board.current_player)[1]
+            print(f"[Win!!!] {player_name}({player_stone}) 님이 오목을 달셩하였습니다.")
+            quit()
+        Board.switch_player()
 
 
 if __name__ == '__main__':
--- modules/settings.py
+++ modules/settings.py
@@ -1,4 +1,4 @@
-board_size = 15
+BOARD_SIZE = 15
 
 MARK_PLAYER1 = '●'
 MARK_PLAYER2 = '○'
@@ -7,17 +7,17 @@ QUIT_KEY = 'Q'
 
 
 # Command Input Messages
-CMD_NAME_PLAYER1 = 'Please Enter the player1 name : '
-CMD_NAME_PLAYER2 = 'Please Enter the player2 name : '
-CMD_BOARD_SIZE = 'Please Enter the Board Size : '
-CMD_PLACE = 'Please Enter the place to put a stone. If enter \'Q\', quit game : '
+CMD_NAME_PLAYER1 = '사용자1 이름 : '
+CMD_NAME_PLAYER2 = '사용자2 이름 : '
+CMD_PLACE = '바둑돌 위치를 공백을 두고 입력하세요. (종료 커맨드-Q) : '
 
 
 # Inform Messages
-MSG_GAME_START = 'Start the Game~!'
-MSG_QUIT = 'Quit the Game'
+MSG_GAME_START = '오목 게임 시작~!'
+MSG_QUIT = '게임을 종료 하였습니다.'
 
 
 # Error Messages
-ERROR_BOARD_SIZE = '[ERROR] Invalid board size. valid range is 5 to 19'
-ERROR_PLACE = '[ERROR] Invalid place. please enter the valid place'
+ERROR_PLACE_INVALID_RANGE = "[ERROR] 바둑판 크기를 벗어난 위치는 입력할 수 없습니다."
+ERROR_PLACE_DUPLICATED = "[ERROR] 이미 바둑돌이 존재하는 위치입니다."
+ERROR_FORMAT = "[ERROR] 바둑돌 위치 입력 형식은 '행인덱스 열인덱스' 입니다."
