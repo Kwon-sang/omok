a7ecaa67acc35a15d9c51b5944baeb163b168495
Kwon Sang <kwonsg514@gmail.com>
2024-02-12 20:08:35 +0900

    test: 유저 입력 테스트

--- src/user_input.py
+++ src/user_input.py
@@ -5,7 +5,7 @@ class UserInput:
     @classmethod
     def board_size(cls) -> int:
         size_input = input("바둑판 크기를 입력하세요 : ")
-        if not re.match('[0-9]+', size_input):
+        if not re.match('^[0-9]+$', size_input):
             raise ValueError("Invalid input")
         return int(size_input)
 
@@ -22,7 +22,7 @@ class UserInput:
         position_input = input("위치를 입력하세요(종료 커맨드-Q) : ")
         if position_input == 'Q':
             raise SystemExit("게임을 종료 합니다.")
-        if not re.match('[0-9]+\s[0-9]+', position_input):
+        if not re.match('^[0-9]+\s[0-9]+$', position_input):
             raise ValueError("Invalid input")
         x, y = position_input.split()
         return int(x), int(y)
